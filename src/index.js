15cf53b1075babbc0f3ccbe8ada56af3061cf53a
DenKorablev <denis18051990@mail.ru>
2018-11-16 17:03:12 +0300

    react

--- src/index.js
+++ src/index.js
@@ -2,11 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+
+    btnClick(event){
+        alert(event.target);
+    }
+
     render() {
-        return (<h1>App Works!!!</h1>);
+        return (
+            <div className="test">
+                <h1>App Works!!!</h1>
+                <h3>It's really working</h3>
+                <button onClick={this.btnClick}>{this.props.children}</button>
+            </div>
+        );
     }
 }
 
 ReactDOM.render(
-    <App></App>, document.getElementById('app')
+    <App>It's prop</App>, 
+    document.getElementById('app')
 )
